d9b9a5b0569c31de1675463d57c94ed43fe21166
Marcel Zak <marcel@zak.codes>
2022-02-20 14:40:12 +0000

    Add ChessCoordinates class.

---
+++ ChessProject-Csharp/src/Classes/ChessCoordinates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace src.Classes
+{
+    public class ChessCoordinates
+    {
+        private int _x;
+        private int _y;
+        public ChessCoordinates(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X
+        {
+
+            get => _x;
+
+            set
+            {
+                if (value is >= 0 and <= 7)
+                {
+                    _x = value;
+                }
+            }
+        }
+
+        public int Y
+        {
+            get => _y;
+
+            set
+            {
+                if (value is >= 0 and <= 7)
+                {
+                    _y = value;
+                }
+            }
+        }
+
+        public override string ToString() => $"(X: {X}, Y: {Y})";
+    }
+}
